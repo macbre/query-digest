9737c44f558e5a1752eec7e20d386c194fa88e03
macbre <maciej.brencz@gmail.com>
2017-06-27 10:25:20 +0200

    get_query_metadata: handle multiple tables in SELECT FROM and JOINs

--- digest/query_metadata.py
+++ digest/query_metadata.py
@@ -17,16 +17,29 @@ def get_query_metadata(query):
 
     try:
         # SELECT FROM, INSERT INTO, DELETE FROM
-        matches = re.search(r'(FROM|INTO) ([`\w]+)', query, flags=re.IGNORECASE)
-        return kind, (matches.group(2).strip('`'),)
-    except:
+        matches = re.search(r'(FROM|INTO) ([`,.\w]+)', query, flags=re.IGNORECASE)
+
+        # multi-table SELECTS
+        # SELECT * FROM foo,bar,test
+        tables = matches.group(2).split(',')
+
+        # now it's time to handle JOINs
+        matches = re.findall(r'JOIN ([`,.\w]+) ON', query, flags=re.IGNORECASE)
+        if matches:
+            tables += matches
+
+        # table names cleanup
+        tables = map(lambda table: table.replace('`', ''), tables)
+
+        return kind, tuple(tables)
+    except AttributeError:
         pass
 
     try:
         # UPDATE foo SET ...
         matches = re.search(r'([`\w]+) SET', query, flags=re.IGNORECASE) if kind == 'UPDATE' else None
         return kind, (matches.group(1).strip('`'),)
-    except:
+    except AttributeError:
         pass
 
     raise ValueError('Could not get metadata for ' + query)
