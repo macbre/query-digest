fa38e8063b8d1f061506780faa7656d4ee77e048
macbre <maciej.brencz@gmail.com>
2017-10-24 09:49:59 +0200

    indexdigest.math.median - make it Python3 compatible

--- digest/math.py
+++ digest/math.py
@@ -4,17 +4,17 @@ def median(data_list):
 
     :type data_list list
     """
-    data_list = map(float, data_list)
+    data_list = list(map(float, data_list))
     n = len(data_list)
     data_list.sort()
     # Test whether the n is odd
     if n & 1:
         # If is is, get the index simply by dividing it in half
-        index = n / 2
+        index = int(n / 2)
         return data_list[index]
     else:
         # If the n is even, average the two values at the center
-        low_index = n / 2 - 1
-        high_index = n / 2
+        low_index = int(n / 2) - 1
+        high_index = int(n / 2)
         average = (data_list[low_index] + data_list[high_index]) / 2
         return average
