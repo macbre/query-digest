f963002dcb53435a8b89a8f37f734a2a7cf761f4
macbre <maciej.brencz@gmail.com>
2018-03-14 18:20:48 +0100

    Update pip dependencies

--- setup.py
+++ setup.py
@@ -11,12 +11,12 @@ setup(
     author_email='maciej.brencz@gmail.com',
     install_requires=[
         'docopt==0.6.2',
-        'tabulate==0.7.7',
+        'tabulate==0.8.2',
         # 'wikia.common.kibana==2.1.2',
         "elasticsearch>=5.0.0,<6.0.0",
         "python-dateutil==2.2",
-        'pytest==3.2.3',
-        'sql_metadata==1.0',
+        'pytest==3.4.2',
+        'sql_metadata==1.1.2',
     ],
     include_package_data=True,
     entry_points={
