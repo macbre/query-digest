adca5dfab81a31571147cb8feac22e5ce2a3923a
macbre <maciej.brencz@gmail.com>
2018-04-07 20:12:51 +0200

    kibana: pylint suggested fixes

--- digest/kibana.py
+++ digest/kibana.py
@@ -38,7 +38,8 @@ class Kibana(object):
     ELASTICSEARCH_HOST = 'logs-prod.es.service.sjc.consul'  # ES5
 
     """ Interface for querying Kibana's storage """
-    def __init__(self, since=None, period=900, es_host=None, read_timeout=10, index_prefix='logstash-other'):
+    def __init__(self, since=None, period=900, es_host=None,
+                 read_timeout=10, index_prefix='logstash-other'):
         """
         :type since int
         :type period int
@@ -47,11 +48,14 @@ class Kibana(object):
         :type index_prefix str
 
         :arg since: UNIX timestamp data should be fetched since
-        :arg period: period (in seconds) before now() to be used when since is empty (defaults to last 15 minutes)
+        :arg period: period (in seconds) before now() to be used when since is empty
+        (defaults to last 15 minutes)
         :arg es_host: customize Elasticsearch host(s) that should be used for querying
         :arg read_timeout: customize Elasticsearch read timeout (defaults to 10 s)
         """
-        self._es = Elasticsearch(hosts=es_host if es_host else self.ELASTICSEARCH_HOST, timeout=read_timeout)
+        self._es = Elasticsearch(
+            hosts=es_host if es_host else self.ELASTICSEARCH_HOST,
+            timeout=read_timeout)
 
         self._logger = logging.getLogger('kibana')
 
@@ -62,7 +66,8 @@ class Kibana(object):
             since = now - period
         else:
             since += 1
-            self._logger.info("Using provided %s timestamp as since (%d seconds ago)", since, now - since)
+            self._logger.info("Using provided %s timestamp as since (%d seconds ago)",
+                              since, now - since)
 
         self._since = since
         self._to = now - self.SHORT_DELAY  # give logs some time to reach Logstash
@@ -88,7 +93,9 @@ class Kibana(object):
         tz_info = tz.tzutc()
 
         # ex. logstash-other-2017.05.09
-        return "{prefix}-{date}".format(prefix=prefix, date=datetime.fromtimestamp(timestamp, tz=tz_info).strftime('%Y.%m.%d'))
+        return "{prefix}-{date}".format(
+            prefix=prefix,
+            date=datetime.fromtimestamp(timestamp, tz=tz_info).strftime('%Y.%m.%d'))
 
     @staticmethod
     def format_timestamp(timestamp):
@@ -130,23 +137,27 @@ class Kibana(object):
         }
 
         # add @timestamp range
-        # @see http://stackoverflow.com/questions/40996266/elasticsearch-5-1-unknown-key-for-a-start-object-in-filters
         # @see https://discuss.elastic.co/t/elasticsearch-watcher-error-for-range-query/70347/2
         body['query']['bool']['must'].append(self._get_timestamp_filer())
 
-        self._logger.debug("Running {} query (limit set to {:d})".format(json.dumps(body), body.get('size', 0)))
+        self._logger.debug("Running %s query (limit set to %d)",
+                           json.dumps(body), body.get('size', 0))
 
-        # use Scroll API to be able to fetch more than 10k results and prevent "search_phase_execution_exception":
-        # "Result window is too large, from + size must be less than or equal to: [10000] but was [500000].
+        # use Scroll API to be able to fetch more than 10k results
+        # and prevent "search_phase_execution_exception":
+        # "Result window is too large, from + size must be less
+        # than or equal to: [10000] but was [500000].
         # See the scroll api for a more efficient way to request large data sets."
         #
         # @see http://elasticsearch-py.readthedocs.io/en/master/helpers.html#scan
         rows = scan(
             client=self._es,
-            clear_scroll=False,  # True causes "403 Forbidden: You don't have access to this resource"
+            # True causes "403 Forbidden: You don't have access to this resource"
+            clear_scroll=False,
             index=self._index,
             query=body,
-            sort=["_doc"],  # return the next batch of results from every shard that still has results to return.
+            # return the next batch of results from every shard that still has results to return.
+            sort=["_doc"],
             size=self.SCROLL_BATCH_SIZE,
         )
 
