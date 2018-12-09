abb1da74c173b28e9ed24f0918b75806986d6fab
macbre <maciej.brencz@gmail.com>
2018-12-09 12:37:27 +0100

    Test table output for file SQL log

--- test/test_query_digest_script.py
+++ test/test_query_digest_script.py
@@ -1,7 +1,12 @@
 from pytest import raises
 
+from os.path import dirname, join
+from io import StringIO
+
 from digest.errors import QueryDigestCommandLineError, QueryDigestReadError
 from scripts.query_digest import main
+
+fixtures_dir = join(dirname(__file__), 'fixtures')
 
 
 def test_required_args():
@@ -19,3 +24,13 @@ def test_read_file_not_found():
         "QueryDigestReadError: [Errno 2] "
         "No such file or directory: '/foo/var/not_existing.sql'"
     )
+
+
+def test_read_file_table():
+    out = StringIO()
+    main(arguments={'--file': join(fixtures_dir, 'queries.sql')}, output=out)
+
+    print(out.getvalue())
+    assert 'test/fixtures/queries.sql" file, found 3 queries' in out.getvalue()
+    assert 'SELECT foo FROM bar WHERE foo = N;' in out.getvalue()
+    assert 'get_items.sql' in out.getvalue()
